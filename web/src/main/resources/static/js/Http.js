9f6f8781c36156d101f9d8c76324a75334fb17ee
li <1161889163@qq.com>
2019-08-19 22:53:47 +0800

    host

--- web/src/main/resources/static/js/Http.js
+++ web/src/main/resources/static/js/Http.js
@@ -1,8 +1,8 @@
-var host = window.location.host;
+var url =window.location.protocol +"//" + window.location.host;
 function postJson(request,success,error) {
     $.ajax({
         type: 'POST',
-        url: host + "/tool/handleString?action=readQRCode",
+        url: url + "/tool/handleString?action=readQRCode",
         contentType:"application/json;charset=utf-8",
         data: JSON.stringify(request),
         success:function (data,status) {
