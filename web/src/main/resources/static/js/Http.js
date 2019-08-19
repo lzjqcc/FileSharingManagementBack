96c95dc1d72ba7caf5fb50169978957745b783fc
li <1161889163@qq.com>
2019-08-19 22:40:37 +0800

    post

---
+++ web/src/main/resources/static/js/Http.js
@@ -0,0 +1,17 @@
+var host = window.location.host;
+function postJson(request,success,error) {
+    $.ajax({
+        type: 'POST',
+        url: window.location.host + "/tool/handleString?action=readQRCode",
+        contentType:"application/json;charset=utf-8",
+        data: JSON.stringify(request),
+        success:function (data,status) {
+            success(data,status)
+        },
+        error:function (data,status) {
+            if (error == null) {
+                alert(data.body)
+            }
+        }
+    });
+}
