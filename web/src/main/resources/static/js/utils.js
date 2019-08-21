44f67cc21a5ee8f9223e159dbebc87be3e88a7a0
li <1161889163@qq.com>
2019-08-21 22:23:26 +0800

    二维码下载

--- web/src/main/resources/static/js/utils.js
+++ web/src/main/resources/static/js/utils.js
@@ -26,3 +26,33 @@ function utf16to8(str) {
     }
     return out;
 }
+
+//下载
+function downloadFile(imageName, base64Image) {
+    var aLink = document.createElement('a');
+    var blob = this.base64ToBlob(base64Image); //new Blob([content]);
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click", true, true);//initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
+    aLink.download = imageName;
+    aLink.href = URL.createObjectURL(blob);
+    // aLink.dispatchEvent(evt);
+    aLink.click()
+}
+//base64转blob
+function base64ToBlob(code) {
+    var parts = code.split(';base64,');
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+    return new Blob([uInt8Array], {type: contentType});
+}
+
+function createQrCode(content) {
+
+}
