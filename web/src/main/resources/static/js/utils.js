76d1dc75f5c9885e86953f54f18a855ea065c660
lizhijian <lizhijian@mytian.com>
2019-08-26 18:00:06 +0800

    中文乱码

--- web/src/main/resources/static/js/utils.js
+++ web/src/main/resources/static/js/utils.js
@@ -26,6 +26,54 @@ function utf16to8(str) {
     }
     return out;
 }
+function utf82str(utf)
+{
+    var str = "";
+    var tmp;
+
+    for(var i = 0; i < utf.length; i++)
+    {
+        // 英文字符集合
+        if(utf.charCodeAt(i) >> 7 == 0x00)
+        {
+            str += utf.charAt(i);
+            continue;
+        }
+        // 其他字符集
+        else if(utf.charCodeAt(i) >> 5 == 0x06)
+        {
+            tmp = ((utf.charCodeAt(i + 0) & 0x1f) << 6) |
+                ((utf.charCodeAt(i + 1) & 0x3f) << 0);
+            str += String.fromCharCode(tmp);
+            i++;
+            continue;
+        }
+        // 中文字符集
+        else if(utf.charCodeAt(i) >> 4 == 0x0e)
+        {
+            tmp = ((utf.charCodeAt(i + 0) & 0x0f) << 12) |
+                ((utf.charCodeAt(i + 1) & 0x3f) <<  6) |
+                ((utf.charCodeAt(i + 2) & 0x3f) <<  0);
+            str += String.fromCharCode(tmp);
+            i += 2;
+            continue;
+        }
+        // 其他字符集
+        else if(utf.charCodeAt(i) >> 3 == 0x1f)
+        {
+            tmp = ((utf.charCodeAt(i + 0) & 0x07) << 18) |
+                ((utf.charCodeAt(i + 1) & 0x3f) << 12) |
+                ((utf.charCodeAt(i + 2) & 0x3f) <<  6);
+            ((utf.charCodeAt(i + 3) & 0x3f) <<  0);
+            str += String.fromCharCode(tmp);
+            i += 3;
+            continue;
+        }
+        str += utf[i];
+    }
+
+    return str;
+}
 
 //下载
 function downloadFile(imageName, base64Image) {
